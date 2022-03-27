f89f65f769f94223b492f2ce79319747888e6c74
erboland <karshigaerbol@gmail.com>
2022-03-27 14:27:33 +0600

    Added courses

--- src/pages/myCourses.jsx
+++ src/pages/myCourses.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { Card, Row } from "react-bootstrap";
+import { Card, Row, Button } from "react-bootstrap";
 import "./mainTable.css";
 export default class MyCourses extends Component {
   constructor(props) {
@@ -14,22 +14,34 @@ export default class MyCourses extends Component {
     this.setState({
       courses: [
         {
-          name: "First",
-          description: "Simple description",
+          name: "Язык Solidity и смарт-контракты Ethereum",
+          description:
+            "Поговорим о языке Solidity, использующемся для написания смарт-контрактов Ethereum. Также поговорим о Remix IDE, напишем первый контракт, развернём его в тестовом блокчейне и попробуем перевести на него денежные средства через Metamask.",
+          url: "https://youtu.be/DgsnHmNukv8",
+          imgUrl:
+            "https://i.ytimg.com/vi/DgsnHmNukv8/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAdEveSKWZDyVWwssyBe0BVJgLVPw",
           course_id: "1",
           subject_name: "math",
           course_rating: "4.5",
         },
         {
-          name: "Second",
-          description: "Simple description",
+          name: "Linear Algebra Done Right",
+          description:
+            "Introduction to the series of videos on my book Linear Algebra Done Right.",
+          url: "https://youtu.be/lkx2BJcnyxk",
+          imgUrl:
+            "https://i.ytimg.com/vi/lkx2BJcnyxk/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCLmbVDN7UGr27aqN_C4fHXw4N2ew",
           course_id: "2",
           subject_name: "math",
           course_rating: "4",
         },
         {
-          name: "Third",
-          description: "Simple description",
+          name: "Urban Economics",
+          description:
+            "Urban economics is a course to learn the economicss of the cities, conglomerates, and corporations",
+          url: "https://youtu.be/jakevMrCbJY",
+          imgUrl:
+            "https://i.ytimg.com/vi/jakevMrCbJY/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBKddltbNYwHcYBrzFq7WDxwjObOA",
           course_id: "3",
           subject_name: "math",
           course_rating: "5",
@@ -44,10 +56,20 @@ export default class MyCourses extends Component {
           {this.state.courses.map((course) => {
             return (
               <Card style={{ marginRight: "1rem" }}>
-                <Card.Img variant="top" src="https://picsum.photos/200" />
+                <Card.Img
+                  variant="top"
+                  src={course.imgUrl}
+                  style={{ marginBottom: "1rem " }}
+                />
                 <Card.Body>
                   <Card.Title>{course.name}</Card.Title>
                   <Card.Text>{course.description}</Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => (window.location = course.url)}
+                  >
+                    Пройти на курс
+                  </Button>
                 </Card.Body>
               </Card>
             );
--- src/pages/newCourse.jsx
+++ src/pages/newCourse.jsx
@@ -28,87 +28,93 @@ export default class NewCourse extends Component {
 
   render() {
     return (
-      <Container className="wrapper">
-        <Form>
-          <Form.Group as={Col} controlId="formName">
-            <Form.Label className="label">Имя курса</Form.Label>
-            <Form.Control
-              type="text"
-              defaultValue={this.state.name}
-              name="name"
-              required
-              onChange={(e) => this.setState({ name: e.target.value })}
-            />
-          </Form.Group>
+      <Container
+        className="wrapper"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <div className="analytics-card">
+          <h2>Добавить курс</h2>
+          <Form>
+            <Form.Group as={Col} controlId="formName">
+              <Form.Label className="label">Имя курса</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={this.state.name}
+                name="name"
+                required
+                onChange={(e) => this.setState({ name: e.target.value })}
+              />
+            </Form.Group>
 
-          <Form.Group as={Col} controlId="formDescription">
-            <Form.Label className="label">Описание курса</Form.Label>
-            <Form.Control
-              type="text"
-              defaultValue={this.state.description}
-              name="description"
-              required
-              onChange={(e) => this.setState({ description: e.target.value })}
-            />
-          </Form.Group>
-          <Form.Group controlId="formLinks">
-            <Form.Label className="label">Ссылки на уроки</Form.Label>
-            {this.state.links.map((link, id) => {
-              console.log(link);
-              return (
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginBottom: "1rem",
-                  }}
-                >
-                  <span style={{ marginRight: "1rem", fontSize: "1.2rem" }}>
-                    {id + 1}.
-                  </span>
-                  <Form.Control
-                    type="url"
-                    defaultValue={link}
-                    name="links"
-                    required
-                    onChange={(e) => this.handleLinks(e, id)}
-                    key={id}
-                  />
-                </div>
-              );
-            })}
-          </Form.Group>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <Button
-              variant="primary"
-              style={{
-                background: "#080808",
-                border: "#080808",
-                color: "#83C18E",
-                marginBottom: "1rem",
-                padding: "0.5rem",
-              }}
-              onClick={() =>
-                this.setState({ links: [...this.state.links, ""] })
-              }
-            >
-              Добавить урок
-            </Button>
+            <Form.Group as={Col} controlId="formDescription">
+              <Form.Label className="label">Описание курса</Form.Label>
+              <Form.Control
+                type="text"
+                defaultValue={this.state.description}
+                name="description"
+                required
+                onChange={(e) => this.setState({ description: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group controlId="formLinks">
+              <Form.Label className="label">Ссылки на уроки</Form.Label>
+              {this.state.links.map((link, id) => {
+                console.log(link);
+                return (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      marginBottom: "1rem",
+                    }}
+                  >
+                    <span style={{ marginRight: "1rem", fontSize: "1.2rem" }}>
+                      {id + 1}.
+                    </span>
+                    <Form.Control
+                      type="url"
+                      defaultValue={link}
+                      name="links"
+                      required
+                      onChange={(e) => this.handleLinks(e, id)}
+                      key={id}
+                    />
+                  </div>
+                );
+              })}
+            </Form.Group>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <Button
+                variant="primary"
+                style={{
+                  background: "#080808",
+                  border: "#080808",
+                  color: "#83C18E",
+                  marginBottom: "1rem",
+                  padding: "0.5rem",
+                }}
+                onClick={() =>
+                  this.setState({ links: [...this.state.links, ""] })
+                }
+              >
+                Добавить урок
+              </Button>
 
-            <Button
-              variant="primary"
-              onClick={this.saveAndContinue}
-              style={{
-                background: "#080808",
-                border: "#080808",
-                color: "#83C18E",
-                padding: "0.5rem",
-              }}
-            >
-              Submit
-            </Button>
-          </div>
-        </Form>
+              <Button
+                variant="primary"
+                onClick={this.saveAndContinue}
+                style={{
+                  background: "#080808",
+                  border: "#080808",
+                  color: "#83C18E",
+                  padding: "0.5rem",
+                }}
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </div>
       </Container>
     );
   }
